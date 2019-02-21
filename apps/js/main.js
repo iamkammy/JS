167c24ce66ee6ca1ca35450343f4a5db728f2f15
kamgirgit <kamaran.jmi12@gmail.com>
2019-02-21 19:02:01 +0530

    new app made

--- apps/js/main.js
+++ apps/js/main.js
@@ -1,9 +1,13 @@
 function devDetect(){
 hide(document.getElementById('lightdiv'));
-toggle(document.getElementById('device-detector'));
+show(document.getElementById('device-detector'));
 }
 
-
+function stopwatch(){
+    show(document.getElementById('stopwatch'));
+    hide(document.getElementById('lightdiv'));
+    hide(document.getElementById('device-detector'));
+}
 
 document.getElementById('device-button').addEventListener('click', function(){
     var platform = document.getElementById('platform');
@@ -11,6 +15,12 @@ document.getElementById('device-button').addEventListener('click', function(){
     var memory = document.getElementById('memory');
     var platformversion = document.getElementById('platform-verson');
 
+       $("#loader").show();
+   setTimeout(wen, 1900);
+   function wen(){
+    $("#loader").hide();
+    $(".output").show();
+   }
     if(navigator.userAgent.match(/Windows/i)){
        platform.innerHTML = `<b>OS :</b> Its windows`
        vendor.innerHTML = `<b>Vendor: </b> ${navigator.vendor}`;
@@ -37,4 +47,62 @@ function toggle(elem){
     } else{
         elem.style.display = "block";
     }
+}
+
+
+// for stopwatch
+var ms=0, s=0, m=0;
+var timer;
+
+var stopwatchelement = document.querySelector('.stopwatch-text');
+var lapseContainer = document.querySelector('.laps');
+
+function start(){
+    if(!timer){
+        timer = setInterval(run, 10);
+    }
+}
+
+function run(){
+    stopwatchelement.innerText = getTimer();
+    ms++;
+    if(ms == 100){
+        ms= 0;
+        s++;
+    }
+    if( s == 60){
+        s=0;
+        m++;
+    }
+}
+function pause(){
+    stopTimer();
+}
+function stop(){
+    stopTimer();
+    ms=0;
+    s=0;
+    m=0;
+    stopwatchelement.innerText = getTimer();
+}
+function stopTimer(){
+    clearInterval(timer);
+    timer = false;
+}
+function getTimer(){
+    return  (m < 10 ? "0"+m: m) + ":" + (s < 10 ? "0"+s: s)  + ":" + (ms < 10 ? "0"+ms : ms); 
+}
+function restart(){
+    stop();
+    start();
+}
+function lap(){
+    if(timer){
+        var li = document.createElement('li');
+        li.innerText = getTimer();
+        lapseContainer.appendChild(li);
+    }
+}
+function resetLaps(){
+    lapseContainer.innerHTML = '';
 }
