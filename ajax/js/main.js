0daad15ab257b57173397b6bf51d98524f6679b0
kamgirgit <kamaran.jmi12@gmail.com>
2019-03-19 19:39:04 +0530

    new

---
+++ ajax/js/main.js
@@ -0,0 +1,25 @@
+$(function(){
+
+var container = $('div.container');
+	$('button#get').click(function(){
+
+				$.ajax({
+					type: 'GET',
+					url: 'data/countries.json',
+					dataType: 'json',
+					cache: false,
+					success: function(data){
+						console.log(data);
+
+					// 	$.each(data, function(index, item){
+					// 		console.log(item);
+					// 	});
+					},
+					error:function(data){ 
+						console.log(data)
+					}
+				});
+				alert("daat");
+	});
+
+});
